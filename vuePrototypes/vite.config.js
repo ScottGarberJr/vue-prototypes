383953c07b6cd08c7a2b6d2140b6b3070737e751
Scott G <scottgarberjr1@gmail.com>
2023-01-07 11:22:42 -0500

    fixed deploy base in vite config

--- vuePrototypes/vite.config.js
+++ vuePrototypes/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vuePrototypes/',
+  // base: '/vuePrototypes/',
   plugins: [vue()],
   resolve: {
     alias: {
